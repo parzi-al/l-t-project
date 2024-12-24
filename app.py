75c5255553c35e6f2a3834e0596bb2d4c3e36114
Bhavan S <109290304+parzi-al@users.noreply.github.com>
2024-12-24 12:20:32 +0530

    visualization fixed

--- app.py
+++ app.py
@@ -139,4 +139,4 @@ def index():
     return render_template("index.html", predictions=predictions, show_images=show_images, error=error)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=6000, debug=True)
